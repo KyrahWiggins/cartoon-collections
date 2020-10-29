cf28cbfd68be35b1c89edb377687e1ae72a19461
KyrahWiggins <kyrah.wiggins@gmail.com>
2020-10-29 11:34:28 -0400

    commit tree f28a55cb7aa52bb0c2f4423003eb738571226757

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |val, index|
+    puts "#{index+1}. #{val}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(names)
+  names.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(names)
+  answer = false
+   names.each do |name|
+     if name.length > 4
+       answer = true
+     end
+   end
+   answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+   cheese.find do |cheese|
+     cheese_types.include?(cheese)
+   end
 end
